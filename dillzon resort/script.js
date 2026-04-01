cf8f8995321ce74b3ac2c5abf6eacced668b865e
Abhay soni <techwolfabhay@gmail.com>
2026-04-02 01:02:04 +0530

    Update script.js

--- dillzon resort/script.js
+++ dillzon resort/script.js
@@ -4,7 +4,7 @@
 const DEF = {
   password: 'dillzan2025',
   mapUrl: 'https://maps.google.com/?q=The+Dillzan+Resort+Alibaug+Mandwa+Jetty',
-  contact: { email:'info@dillzanresort.com', address:'Near Ranavali Farm House And Resort, At: Rahatale, Post: Dhokawade, Ta:, Mandwa Jetty Rd, Rahatale, Alibag, Maharashtra 402208', whatsapp:'917715966111' },
+  contact: { email:'info@thedillzanresort.com', address:'Near Ranavali Farm House And Resort, At: Rahatale, Post: Dhokawade, Ta:, Mandwa Jetty Rd, Rahatale, Alibag, Maharashtra 402208', whatsapp:'917715966111' },
   social: { whatsapp:'917715966111', instagram:'https://www.instagram.com/the_dilzan_resort?utm_source=qr&igsh=MXcwcDdtNTY0OHBwZA==', igHandle:'@the_dilzan_resort', facebook:'https://www.facebook.com/dillzanresort' },
   amenities: [
     {id:1,icon:'fa-swimming-pool',name:'Swimming Pool',desc:'Beautiful pool to relax and unwind'},
